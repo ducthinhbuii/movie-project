37dac12c5d953c5e00a1b8a3dc9bf43491ff8fb5
freeredfingerlan5 <freeredfingerlan5@gmail.com>
2024-06-10 12:34:42 +0000

    update

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import './styles.scss'
 
+import {ContentWrapper} from '../../components/contentWrapper/ContentWrapper'
+
 export const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+    <div className="pageNotFound">
+        <ContentWrapper>
+            <span className="bigText">404</span>
+            <span className="smallText">Page not found!</span>
+        </ContentWrapper>
+    </div>
   )
 }
